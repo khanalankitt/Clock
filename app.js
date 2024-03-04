ef04e8e02606134c659f5c021d358bff66ca6e98
Ankit Khanal <akhanal749@gmail.com>
2024-03-04 20:54:33 +0545

    Created Clock

---
+++ app.js
@@ -0,0 +1,21 @@
+document.querySelector('.main').style.transform = 'rotate(180deg)';
+let a = setInterval(()=>{  
+    let date = new Date();
+    let tempsec = date.getSeconds();
+    let tempmin = date.getMinutes();
+    let temphr = date.getHours();
+    let amorpm = (temphr <12) ? "AM" : "PM";
+    temphr %= 12;
+    (tempsec < 10)? tempsec = "0" + tempsec : tempsec;
+    (tempmin < 10)? tempmin = "0" + tempmin : tempmin;
+    (temphr < 10 )? temphr = "0" + temphr : temphr;
+    document.querySelector('.h1').innerHTML = ` ${temphr}:${tempmin}:${tempsec} ${amorpm}`;
+    let sec = date.getSeconds()*6;
+    let minutepercent = tempmin/60;
+    let secondpercent = tempsec/360;
+    let hour = (date.getHours()*12) + (date.getHours()*minutepercent);
+    let minute = (date.getMinutes()*6) + (date.getMinutes() * secondpercent);
+    document.querySelector(".second").style.transform = `rotate(${sec}deg)`;
+    document.querySelector(".hour").style.transform = `rotate(${hour}deg)`;
+    document.querySelector(".minute").style.transform = `rotate(${minute}deg)`;
+ },100);
